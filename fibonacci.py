a044452da76b64233fb693e8cae5a68aaa692ede
Jonah Ortiz <jortiz06@uoguelph.ca>
2025-10-22 11:44:19 -0400

    Implement Fibonacci sequence functions
    Added functions to get user input and generate Fibonacci sequence.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,29 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+def get_input():
+  input = 0
+  while (True):
+    input = input("Enter a positive integer: ")
+    if input.isdigit():
+      if int(input) > 0:
+        return int(input)
+      else:
+        print("Enter a valid integer!")
+    else:
+      print("Enter a valid integer!")
+
+def generate_fibonacci(terms):
+  i = 1
+  j = 0
+  tmp = 0
+  sequence = ""
+  for i in range(terms):
+    sequence = sequence + "{i} "
+    tmp = i
+    i = i + j
+    j = tmp
+
+def print_sequence(sequence):
+  print("{sequence}")
